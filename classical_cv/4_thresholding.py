4541a57626515766d2746ce26c83a34328f1bf3a
novoforce <ashish.space5@gmail.com>
2022-01-25 19:15:06 +0530

    morphology added

--- classical_cv/4_thresholding.py
+++ classical_cv/4_thresholding.py
@@ -37,6 +37,29 @@ def binary_threshold_to_zero_inverse(image):
     cv2.imwrite('./binary_threshold_to_zero_inv.png',thresh2)
 
 
+def adaptive_threshold(image):
+    img = cv2.imread(image,0)
+    # display(img)
+
+    #simple thresholding
+    ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+    # display(th1)
+
+
+    #adaptive thresholding
+    th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,8)
+    # display(th2)
+
+    th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,15,8)
+    # display(th3)
+
+
+    blended = cv2.addWeighted(src1=th2,alpha=0.7,src2=th1,beta=0.3,gamma=0)
+    # blended= cv2.bitwise_and(th2,th3)
+    display(blended)
+
+
+
 
 
 
@@ -53,8 +76,8 @@ def binary_threshold_to_zero_inverse(image):
 
 if __name__ == '__main__':
     img_filename= r'images/rainbow.jpg'
-    img = cv2.imread(img_filename,0) #read the image in B/W format
-    display(img)
+    # img = cv2.imread(img_filename,0) #read the image in B/W format
+    # display(img)
     # while True:
     #     cv2.imshow('golden_retriever',img)
     #     #if i waited for 1 sec and i pressed q then break
@@ -70,4 +93,10 @@ if __name__ == '__main__':
 
     # binary_threshold_to_zero(img)
 
-    binary_threshold_to_zero_inverse(img)
+    # binary_threshold_to_zero_inverse(img)
+
+    ##############################################################################333
+    img_file= r"D:\Exploring-Tensorflow\classical_cv\images\crossword.jpg"
+    adaptive_threshold(img_file)
+
+
